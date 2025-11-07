19db631b5122190e1222ca45c88681ae0154ae56
crxzysheep <98056142+crxzysheep@users.noreply.github.com>
2025-11-07 00:35:57 -0500

    fix bugs

--- no more cheese/Assets/Scripts/PlayerMovement.cs
+++ no more cheese/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
 
-public class PlayerMovement
+public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float speed;
     private Rigidbody2D body;
 
-    private void Awake()
+    public void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
     }
 }

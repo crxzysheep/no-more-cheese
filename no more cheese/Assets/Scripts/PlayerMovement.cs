e66aeca07936ad1ea7f9736581456b78bc5667f7
crxzysheep <98056142+crxzysheep@users.noreply.github.com>
2025-11-09 16:58:11 -0500

    better movement

--- no more cheese/Assets/Scripts/PlayerMovement.cs
+++ no more cheese/Assets/Scripts/PlayerMovement.cs
@@ -2,42 +2,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private float jumpCutoff;
+    [SerializeField] private float jumpBuffer;
     private Rigidbody2D body;
-    private bool grounded;
+
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
     public void Awake()
     {
         body = GetComponent<Rigidbody2D>();
     }
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-    
     // Update is called once per frame
     void Update()
     {
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, body.linearVelocity.y);
 
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetButtonDown("Jump") && isGrounded())
         {
-            
+            jump();
         }
+
+        if(Input.GetButtonUp("Jump") && body.linearVelocity.y > 0f)
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, body.linearVelocity.y * jumpCutoff);
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, jumpBuffer, groundLayer);
     }
 
     private void jump()
     {
-        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
-        grounded = false;
+        body.linearVelocity = new Vector2(body.linearVelocity.x, jumpPower);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
-            grounded = true;
     }
 }
